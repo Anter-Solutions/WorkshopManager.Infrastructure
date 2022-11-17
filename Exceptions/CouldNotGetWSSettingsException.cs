c07ed1f766e0b85ab2f22555914258c2f8460f4b
vladimirmazarakis <87600021+vladimirmazarakis@users.noreply.github.com>
2022-11-18 00:49:25 +0200

    Exceptions

---
+++ Exceptions/CouldNotGetWSSettingsException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkshopManager.Infrastructure.Exceptions
+{
+    public class CouldNotGetWSSettingsException : Exception
+    {
+        public CouldNotGetWSSettingsException()
+        {
+        }
+
+        public CouldNotGetWSSettingsException(string? message) : base(message)
+        {
+        }
+
+        public CouldNotGetWSSettingsException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+        protected CouldNotGetWSSettingsException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
---
+++ Exceptions/NoAvailableConstructorException.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkshopManager.Infrastructure.Exceptions
+{
+    public class NoAvailableConstructorException : Exception
+    {
+        public NoAvailableConstructorException()
+        {
+        }
+
+        public NoAvailableConstructorException(string? message) : base(message)
+        {
+        }
+
+        public NoAvailableConstructorException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+        protected NoAvailableConstructorException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+
+
+    }
+}
