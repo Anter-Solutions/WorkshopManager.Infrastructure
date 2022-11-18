1a7621ac423aeaabfc9f0618b0b3153cf23deae1
vladimirmazarakis <87600021+vladimirmazarakis@users.noreply.github.com>
2022-11-18 22:09:59 +0200

    LoginDTO added.

---
+++ DTOs/Auth/LoginDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkshopManager.Infrastructure.DTOs.Auth
+{
+    public record LoginDTO
+    {
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public string Password{ get; set; }
+        public bool DoNotSignOut { get; set; }
+    }
+}
